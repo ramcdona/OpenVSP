dab1b9ba5ae2a26ebafdf1fbb44170f2aaec5a18
Rob McDonald <rob.mcdonald@uber.com>
2020-12-02 12:26:06 -0800

    Add first cuts at VspSurf DegenCamberSurf and DegenPlanarSurf

--- src/util/VspSurf.h
+++ src/util/VspSurf.h
@@ -250,6 +250,9 @@ public:
 
     void MakePlaneSurf( const vec3d &ptA, const vec3d &ptB, const vec3d &ptC, const vec3d &ptD );
 
+    void DegenCamberSurf();
+    void DegenPlanarSurf();
+
 protected:
 
     void Tesselate( const vector<double> &utess, const vector<double> &vtess, std::vector< vector< vec3d > > & pnts,  std::vector< vector< vec3d > > & norms,  std::vector< vector< vec3d > > & uw_pnts ) const;
