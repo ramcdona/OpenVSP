798a7c2bf84e74bd5cef4b19e8fdfd781f15a560
Tristan <tristan.cavarno@esaero.com>
2023-01-23 13:52:21 -0800

    sorted local library issues and renamed a file stub

--- examples/scripts/python_scripts/Master_VSP_VV_Script.py
+++ examples/scripts/python_scripts/Master_VSP_VV_Script.py
@@ -1,9 +1,20 @@
-import ~/PrivateVSP_ESAero/build/python_apivsp.py
+import openvsp as vsp
+
+def Test_All():
+    pass
 
 def main():
-    print("Beging MAseter VSP V&V Script")
-    version = v.getVSPVersion() #GetVSPVersion
-    open(version+"_VV.html")
+    print("Begining MAseter VSP V&V Script")
+    version = vsp.GetVSPVersion() #GetVSPVersion
+    file = open(version+"_VV.html")
+    test = Test_All()
+    errorMgr :vsp.ErrorMgrSingleton = vsp.ErrorMgrSingleton_getInstance()
+
+    #Check for API Errors 
+    while(errorMgr.GetNumTotalErrors() >0):
+        err:vsp.ErrorObj = errorMgr.PopLastError()
+        print(err.GetErrorString())
+
 
     
     print("End Master VSP V&V Script")
