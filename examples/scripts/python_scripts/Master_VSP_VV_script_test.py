fc228a8325faa597c84037b73f15a4f0091f0c14
leparrain777 <perrin.silveira@pacbell.net>
2023-04-28 14:55:55 -0700

    more robust filepathing

--- examples/scripts/python_scripts/Master_VSP_VV_script_test.py
+++ examples/scripts/python_scripts/Master_VSP_VV_script_test.py
@@ -1,14 +1,16 @@
 import subprocess
 import sys
+from pathlib import Path
 
 def main():
-    subprocess.Popen([sys.executable, './SuperDeltaTest.py'],start_new_session=True)
-    subprocess.Popen([sys.executable, './EllipseTest.py'],start_new_session=True)
-    subprocess.Popen([sys.executable, './VKTTest.py'],start_new_session=True)
-    subprocess.Popen([sys.executable, './WarrenTest.py'],start_new_session=True)
-    subprocess.Popen([sys.executable, './BertinSmithTest.py'],start_new_session=True)
-    subprocess.Popen([sys.executable, './SweptTest.py'],start_new_session=True)
-    subprocess.Popen([sys.executable, './HersheyTest.py'],start_new_session=True)
+    currentpath = str(Path(__file__).parent.resolve())
+    subprocess.Popen([sys.executable, currentpath+'/SuperDeltaTest.py'],start_new_session=True)
+    subprocess.Popen([sys.executable, currentpath+'/EllipseTest.py'],start_new_session=True)
+    subprocess.Popen([sys.executable, currentpath+'/VKTTest.py'],start_new_session=True)
+    subprocess.Popen([sys.executable, currentpath+'/WarrenTest.py'],start_new_session=True)
+    subprocess.Popen([sys.executable, currentpath+'/BertinSmithTest.py'],start_new_session=True)
+    subprocess.Popen([sys.executable, currentpath+'/SweptTest.py'],start_new_session=True)
+    subprocess.Popen([sys.executable, currentpath+'/HersheyTest.py'],start_new_session=True)
     return
 
 
