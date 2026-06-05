8c79544808c86d70065e42e32324632cac74d81e
Rob McDonald <rob.a.mcdonald@gmail.com>
2026-06-05 00:09:38 +0000

    Fix memory leak for From Point Viz analysis w/ GUI open.

--- src/geom_core/GeometryAnalysisMgr.cpp
+++ src/geom_core/GeometryAnalysisMgr.cpp
@@ -2594,6 +2594,7 @@ void GeometryAnalysisCase::UpdateDrawObj_Live()
             m_SecondaryVizPointDO.m_PointSize = 20.0;
             m_SecondaryVizPointDO.m_GeomChanged = true;
         }
+        DeleteTMeshVec( fov_vec );
     }
 }
 
